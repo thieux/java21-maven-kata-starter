af33771d9b107f310c966a3f7daad1df80511938
mathieu <mathieu.pauly@gmail.com>
2024-06-18 22:00:47 +0200

    test: add failing test

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    @Test
+    void fails() {
+        assertEquals(1, 0);
+    }
+}
